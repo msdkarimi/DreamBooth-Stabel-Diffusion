a438c2308184bbc0a71bb6ada0186df11d90703d
msdkarimi <itsmasoudkarimi@gmail.com>
2024-06-15 12:53:33 +0200

    Update modules.py

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -153,8 +153,8 @@ class FrozenCLIPEmbedder(AbstractEncoder):
         batch_encoding = self.tokenizer(text, truncation=True, max_length=self.max_length, return_length=True,
                                         return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
         tokens = batch_encoding["input_ids"].to(self.device)
-        print()
-        print(f'tokens = {tokens}')
+        # print()
+        # print(f'tokens = {tokens}')
         outputs = self.transformer(input_ids=tokens)
 
         z = outputs.last_hidden_state
